215fc1e0abad788ead5c35267a6ffb8b6ff4ba23
Sergey <s.melnikov3@g.nsu.ru>
2019-02-15 23:08:40 +0700

    task № 2 added

---
+++ guessing-game/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static string nameOfGamer;
+        const string messageToSayBye = "Bye.";
+        const string messageAboutWrongNumber = "Enter a number from 0 to ";
+        static int secretNumber;
+        static List<int> history = new List<int>();
+        static int counter = 0;
+        static string[] frazes = {", patience.", ", do not give up.",
+             ", you will succeed.", ", victory does not far away."};
+        static Random rand = new Random();
+        const int maxNumber = 50;
+        static DateTime start;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name.");
+            nameOfGamer = Console.ReadLine();
+            secretNumber = rand.Next(0, maxNumber + 1);
+            Console.WriteLine($"Hello, {nameOfGamer}. Guess a number from 0 to {maxNumber}.");
+            start = DateTime.Now;
+            bool work = true;
+            string value;
+
+            while(work) {
+                value = Console.ReadLine();
+                if("q".Equals(value)) {
+                    work = false;
+                    Console.WriteLine(messageToSayBye);
+                }
+                else {
+                    try {
+                        int number = Int32.Parse(value, NumberStyles.Integer, new CultureInfo("ru-RU"));
+                        if(!(number >= 0 && number <= maxNumber)) {
+                            Console.WriteLine($"{messageAboutWrongNumber}{maxNumber}");
+                        } else {
+                            if(processing(number))
+                                work = false;
+                        }
+                    } catch(Exception ex) {
+                        if(ex is FormatException || ex is OverflowException) {
+                            Console.WriteLine($"{messageAboutWrongNumber}{maxNumber}");
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+        static bool processing(int number) {
+            counter++;
+            if(number > secretNumber) {
+                history.Add(-number);
+                Console.WriteLine("More");
+                if(counter % 4 == 0 && counter != 0) {
+                    writeFraze();
+                }
+                return false;
+            }
+            else if(number < secretNumber) {
+                Console.WriteLine("Less");
+                if (counter % 4 == 0 && counter != 0) {
+                    writeFraze();
+                }
+                history.Add(number);
+                return false;
+            }
+            else {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"Congrats! Secret number is {secretNumber}. You win!");
+                Console.ForegroundColor = ConsoleColor.White;
+                showHistory();
+                return true;
+            }
+        }
+        static void showHistory() {
+            if(counter == 0) {
+                Console.WriteLine("On the first try!");
+            } else {
+                Console.WriteLine($"Аttempts {counter}");
+                foreach (int item in history) {
+                    Console.Write($"{Math.Abs(item)}   ");
+                    if(item >= 0) {
+                        Console.WriteLine("Less");
+                    }
+                    else {
+                        Console.WriteLine("More");
+                    }
+                }
+            }
+            DateTime end = DateTime.Now;
+            TimeSpan time = end - start;
+            Console.WriteLine($"Spent time - {time.Hours}:{time.Minutes}:{time.Seconds}");
+        }
+        static void writeFraze () {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{nameOfGamer}{frazes[rand.Next() % frazes.Length]}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
